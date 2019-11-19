74a5a63d56b71292652ca5b3c3414bbb7e66c4a5
Toan Dang <ddtoan991205@gmail.com>
2019-11-19 13:00:18 +0700

    Bai 7 -Event Binding

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -3,12 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', // document.querySelector('app-root); <app-root></app-root>
   template: `
-    <h1>Welcome to Coders Tokyo</h1>
-    <app-hello></app-hello>
-    <app-hi></app-hi>
+    <h1
+      [class.with-border]="withBorder"
+      [style.color]="textColor"
+      (mouseover)="onTextMouseOver()"
+      (mouseout)="onTextMouseOut()">
+        Welcome to {{title}}
+    </h1>
+
+    <button (click)="onButtonClick($event)">{{withBorder ? "Hide border" : "Show border"}}</button>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-coders-tokyo';
+  title = 'Coder Tokyo';
+  imageSrc = 'https://picsum.photos/200';
+
+  textColor = 'tomato';
+  backgroundColor = 'black';
+
+  styleObj = {color: this.textColor, background: this.backgroundColor};
+  withBorder = true;
+
+  onButtonClick(event: MouseEvent){
+    console.log('Button clicked !!');
+    this.withBorder = !this.withBorder;
+  }
+
+  onTextMouseOver(){
+    this.textColor = 'yellow';
+    console.log('1');
+  }
+
+  onTextMouseOut(){
+    console.log('2');
+    this.textColor = 'tomato';
+  }
 }
+
+// DataBinding`
+// 1. PropertyBinding
+// 2. EventBinding
